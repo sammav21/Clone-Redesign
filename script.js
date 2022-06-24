a8453949519fa389784f9bf481857ccd8a5ca826
S.Aviles <sammav96@gmail.com>
2022-06-24 11:23:04 -0400

    DONE
    Mobile and desktop versions completed. Overall this project turned out to be more of a redesign then clone. I kept the general layout of the site (besides the picture on each page) and made use of color and smart design to clear up, brighten, and make the site easier and more efficient to use

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ var x = document.getElementById('x');
 
 employment.addEventListener('click', myFunction);
 x.addEventListener('click', myFunction);
-console.log(x);
 
 function myFunction() {
     var popup = document.getElementById('popup')
     popup.classList.toggle('show');
   }
+
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', dropdown);
+
+function dropdown(){
+    btn.classList.toggle('btn-active');
+    var menu = document.querySelector('.dropdown-menu');
+    menu.classList.toggle('visible');
+}
